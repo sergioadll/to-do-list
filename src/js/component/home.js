5860a676e5778bd5216df0735d7f66e8892ee381
sergioadll <sergio.adll@hotmail.com>
2020-11-03 01:24:44 +0000

    api integrated needs to be checked

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,37 +1,77 @@
 import React from "react";
+import { element } from "prop-types";
 
 //create your first component
-
 export class Home extends React.Component {
 	constructor() {
 		super();
 		//we declare the initial array of objects
 		this.state = {
-			todos: [
-				{ done: false, task: "First task", id: Math.random() * 10 },
-				{ done: false, task: "Second task", id: Math.random() * 10 },
-				{ done: false, task: "Third task", id: Math.random() * 10 }
-			],
-			newTask: ""
+			todos: [],
+			newTask: "",
+			baseUrl: "https://assets.breatheco.de/apis/fake/todos/user/sergio"
 		};
+		this.todos = [];
+	}
+	async loadTodos() {
+		var requestOptions = {
+			method: "GET",
+			redirect: "follow"
+		};
+		try {
+			let res = await fetch(this.state.baseUrl, requestOptions);
+			let result = await res.json();
+			let todoList = result.map((element, index) => {
+				element.id = index;
+				return element;
+			});
+			this.state.todos = todoList;
+			console.log(todoList);
+		} catch (error) {
+			console.log("error", error);
+		}
+	}
+	async modifyTodos() {
+		var requestOptions = {
+			method: "PUT",
+			body: JSON.stringify(this.state.todos),
+			headers: { "Content-Type": "application/json" },
+			redirect: "follow"
+		};
+		try {
+			let res = await fetch(this.state.baseUrl, requestOptions);
+			let result = await res.json();
+			console.log("try", this.state.todos);
+			console.log(result);
+		} catch (error) {
+			console.log("error", error);
+		}
+	}
+	componentDidMount() {
+		this.loadTodos();
 	}
 	addTask(e) {
 		e.preventDefault();
+
 		this.setState({
 			todos: this.state.todos.concat([
 				{
+					label: this.state.newTask,
 					done: false,
-					task: this.state.newTask,
 					id: Math.random() * 10
 				}
-			])
+			]),
+			newTask: ""
 		});
+		this.modifyTodos();
 		return false;
 	}
 	deleteTask(id) {
 		this.setState({
 			todos: this.state.todos.filter(task => task.id != id) //returns all the task but the one that we want to eliminate
 		});
+		let todos = this.state.todos;
+		this.modifyTodos(todos);
 	}
 
 	render() {
@@ -40,7 +80,7 @@ export class Home extends React.Component {
 			return (
 				// returns the task in a lable and a button that calls the function deleteTask when clicked
 				<li className="list-group-item d-flex" key={task.id}>
-					<label>{task.task}</label>
+					<label>{task.label}</label>
 					<button
 						type="submit"
 						className="btn btn-danger btn-sm py-0 ml-auto"
@@ -51,52 +91,29 @@ export class Home extends React.Component {
 			);
 		});
 		return (
-			//section 1 is the header
-			//section 2 imputs the task
-			//section 3 shows the to-do list
 			<div className="container">
-				<div className="row h-100">
-					<div className="col-10 mx-auto ">
-						<div className="col-10 mx-auto">
-							<section>
-								<div className="jumbotron jumbotron-fluid m-0 bg-secondary">
-									<div className="container">
-										<h1 className="display-4 text-white text-center">
-											To-do List
-										</h1>
-									</div>
-								</div>
-							</section>
-							<section>
-								<form
-									className="form-inline"
-									onSubmit={this.addTask.bind(this)}>
-									<div className="col-10 form-group px-0">
-										<input
-											type=""
-											className="form-control w-100"
-											placeholder="Task example"
-											value={this.state.newTask} //we assign newTask to value
-											onChange={e =>
-												this.setState({
-													newTask: e.target.value
-												})
-											} //the event updates the component so newTask gets value's value
-										/>
-									</div>
-									<button
-										type="submit"
-										className="col-2 btn btn-success">
-										Add!
-									</button>
-								</form>
-							</section>
-							<section>
-								<ul className="list-group">{todoList}</ul>
-							</section>
-						</div>
-					</div>
+				<h1 className="display-4 ext-center">To-do List</h1>
+				<div className="d-flex">
+					<input
+						id="input"
+						type=""
+						className="form-control w-100"
+						placeholder="Task example"
+						value={this.state.newTask} //we assign newTask to value
+						onChange={e =>
+							this.setState({
+								newTask: e.target.value
+							})
+						} //the event updates the component so newTask gets value's value
+					/>
+					<button
+						type="submit"
+						className="col-2 btn btn-success"
+						onClick={this.addTask.bind(this)}>
+						Add!
+					</button>
 				</div>
+				<ul className="list-group">{todoList}</ul>
 			</div>
 		);
 	}
